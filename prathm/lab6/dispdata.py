7d96297bc461b88afbdf8801dfcf3f17c745f65b
Prathm Juneja <pjuneja@student00.cse.nd.edu>
2017-03-21 16:23:01 -0400

    brief changes to comment the code for dispdata.py

--- prathm/lab6/dispdata.py
+++ prathm/lab6/dispdata.py
@@ -7,10 +7,11 @@ import requests
 import csv
 
 def opendatafile(filename):
-    with open(filename, 'rb') as csvfile:
-        data = csv.reader(csvfile, delimiter=',', quotechar='|')
+    with open(filename, 'rb') as csvfile: #Import the file
+        data = csv.reader(csvfile, delimiter=',', quotechar='|') #save the files data
         first = data.next()
-        print "".ljust(74, '=')
+        #Start a table formatting to print out the data and make it clear enough to understand
+        print "".ljust(74, '=') 
         print '|' + first[0].ljust(53) + '|' + first[1].ljust(10) + '|' + first[2].ljust(10) + '|'
         print "".ljust(74, '=')
         for row in data:
@@ -19,4 +20,5 @@ def opendatafile(filename):
 
 print("This is a sample program that opens schools.csv file. This csv file comes from the South Bend Open Data website, and contains the names of various schools, the type of school, and the boundary number for the school. It can easily be adapted to open a different open data file and display it in a similar fashion.")
 
+# Import the data file "schools.csv" using python
 opendatafile('schools.csv')
