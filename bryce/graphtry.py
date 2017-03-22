ce0b8dd583bafe5d0968733b404cda6dd32a5f32
Bryce Badura <bbadura@nd.edu>
2017-03-21 23:02:51 -0400

    graphing csv

---
+++ bryce/graphtry.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python2.7
+
+import sys
+import matplotlib.pyplot as plt
+import csv
+
+csv_reader = csv.reader(open('datapts.csv'))
+bigx = float(-sys.maxint -1)
+bigy = float(-sys.maxint -1)
+smallx = float(sys.maxint)
+smally = float(sys.maxint)
+
+verts = []
+for row in csv_reader:
+	verts.append(row)
+	if float(row[0]) > bigx:
+		bigx = float(row[0])
+	if float(row[1]) > bigy:
+		bigy = float(row[1])
+	if float(row[0]) < smallx:
+		smallx = float(row[0])
+	if float(row[1]) < smally:
+		smally = float(row[1])
+
+verts.sort()
+x_arr = []
+y_arr = []
+for vert in verts:
+	x_arr.append(vert[0])
+	y_arr.append(vert[1])
+
+fig = plt.figure()
+ax = fig.add_axes([0.1, 0.1, 0.8, 0.8])
+
+ax.set_xlabel('x data')
+ax.set_ylabel('y data')
+ax.set_xlim(smallx,bigx)
+ax.set_ylim(smally,bigy)
+
+ax.plot(x_arr,y_arr, color='blue', lw=2)
+plt.show()
+fig.savefig('test.png')
